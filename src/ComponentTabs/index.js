a110a8cfabb6c3551555b726b59684cc8e5fece9
Kevin Sherman <sherman.kevin.g@gmail.com>
2020-04-19 00:09:51 +0100

    new options

--- src/ComponentTabs/index.js
+++ src/ComponentTabs/index.js
@@ -21,9 +21,11 @@ const ComponentTab = () => {
     console.log({...appState})
 
     if (selectedRace) {
+        const components = selectedRace.components.sort((a, b) => a.order - b.order);
+        console.log('components', components.map(c => c.name))
         return (
             <Container>
-                {selectedRace.components.sort((a, b) => a.order - b.order).map(component => 
+                {components.map(component => 
                     <Tile onClick={() => selectComponentTab(component.name)}>{component.name}</Tile>
                 )}
             </Container>
